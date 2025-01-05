e8234811a6c9d7d098d24c8958a369a1d1073f3a
nawsish <nawsish.ahmed@spectrum-bd.com>
2025-01-05 15:03:51 +0600

    add db connection

---
+++ config/db_connection.php
@@ -0,0 +1,11 @@
+<?php
+	
+	$dbname = "znz";
+	$hostname = "localhost";
+	$username = "root";
+	$password = "";
+	
+	$password = "";
+	$con = mysqli_connect($hostname, $username, $password, $dbname );
+	mysqli_set_charset($con, "utf8");
+?>
